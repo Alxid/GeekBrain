f1e4969af52c86c386322cb6194869fe88d92650
Aleksey <aleksoid@ddays.ru>
2021-05-17 14:14:10 +0300

    4/5
    only 4 of 5 steps are done.

--- HW2/1.cpp
+++ HW2/1.cpp
@@ -11,6 +11,21 @@ int main() {
     double doubleCount;
     doubleCount = 11.654987;
 
+    enum xol {x, o, m, p};
+    // x - крестик
+    // o - нолик
+    // m - простановка там, где крестики выиграли (замена трех крестиков)
+    // p - простановка там, где нолики выиграли (замена трех ноликов)
+
+    char xow[5] = {'x', 'o', 'm', 'p', '\0'};
+
+    struct game {
+        int playerID[2]; // номер игрока (1, 2)
+        int actID; // номер хода
+        int actData[2]; // дийствие пользователя (х или о)
+        int playerPos[3][3]; // место, куда пользователь установил х или о
+    };
+
     std::cout << a << std::endl;
     std::cout << b << std::endl;
     std::cout << c << std::endl;
