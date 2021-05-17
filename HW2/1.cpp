62078f18b203e4d1479a9f9eeec92d52e0a1b1a6
Aleksey <aleksoid@ddays.ru>
2021-05-17 12:58:23 +0300

    hw2
    First part of second hw is done.

---
+++ HW2/1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+
+    int a = 92;
+    long b = 1123654;
+    short c = -8;
+    unsigned d = 4;
+    char letter = 'B';
+    float count = 1.9865;
+    double doubleCount;
+    doubleCount = 11.654987;
+
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << c << std::endl;
+    std::cout << d << std::endl;
+    std::cout << letter << std::endl;
+    std::cout << count << std::endl;
+    std::cout << doubleCount << std::endl;
+
+    return 0;
+}
