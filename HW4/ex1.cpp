7c20d18cf605561f75e169706c5c767e65d0e1b6
Aleksey <aleksoid@ddays.ru>
2021-05-25 00:18:30 +0300

    Ex 1 is done

--- HW4/ex1.cpp
+++ HW4/ex1.cpp
@@ -2,7 +2,19 @@
 
 int main () {
 
-    std::cout << "Hello, World!!" << std::endl;
+    int a;
+    int b;
+
+    std::cin >> a;
+    std::cin >> b;
+
+    if ((a + b) >= 10 && (a + b) <= 20) {
+        std::cout << "TRUE!" << std::endl;
+    } else {
+        std::cout << "FALSE!" << std::endl;
+    }
+
+
 
     return 0;
 }
